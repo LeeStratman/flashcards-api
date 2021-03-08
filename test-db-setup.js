6c2b92ff18a321d08aca5f1e9c2d2b0590e10561
Lee Stratman <stratmanlc10@gmail.com>
2021-03-08 17:47:35 -0600

    Add clearDB and isDisconnected functions to the test setup file.

--- test-db-setup.js
+++ test-db-setup.js
@@ -1,33 +1,41 @@
 const mongoose = require("mongoose");
 const config = require("config");
 
-function clearDB() {
-  for (var i in mongoose.connection.collections) {
-    mongoose.connection.collections[i].deleteMany(function () {});
+async function clearDB(connection) {
+  for (const hash in connection.collections) {
+    await connection.collections[hash].deleteMany();
   }
 }
 
+const isDisconnected = (connection) => {
+  return connection.readyState === 0 ? true : false;
+};
+
+/**
+ * Runs a function before each of the tests in a file runs.
+ */
 beforeEach(async (done) => {
-  if (mongoose.connection.readyState === 0) {
+  if (isDisconnected(mongoose.connection)) {
     try {
       await mongoose.connect(config.get("mongoURI"), {
         useNewUrlParser: true,
         useUnifiedTopology: true,
+        useFindAndModify: false,
       });
-      clearDB();
+      await clearDB(mongoose.connection);
     } catch (e) {
       console.log("connection error");
       console.error(e);
       throw e;
     }
   } else {
-    clearDB();
+    await clearDB(mongoose.connection);
   }
   done();
 });
 
 afterEach(async (done) => {
-  clearDB();
+  await clearDB(mongoose.connection);
   await mongoose.disconnect();
   return done();
 });

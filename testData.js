9f5126e65876d7e883fb6df1811c1529d167a350
Lee Stratman <stratmanlc10@gmail.com>
2021-03-08 20:34:08 -0600

    Add test data.

---
+++ testData.js
@@ -0,0 +1,74 @@
+const data = [
+  {
+    name: "Physics",
+    flashcards: [
+      {
+        question: "What is Energy?",
+        answer:
+          "The capacity of a physical system to do work or any aspect of native that allows change to occur.",
+      },
+      {
+        question: "What is Energy?",
+        answer:
+          "The capacity of a physical system to do work or any aspect of native that allows change to occur.",
+      },
+      {
+        question: "What is Energy?",
+        answer:
+          "The capacity of a physical system to do work or any aspect of native that allows change to occur.",
+      },
+      {
+        question: "What is Energy?",
+        answer:
+          "The capacity of a physical system to do work or any aspect of native that allows change to occur.",
+      },
+      {
+        question: "What is Energy?",
+        answer:
+          "The capacity of a physical system to do work or any aspect of native that allows change to occur.",
+      },
+      {
+        question: "What is Energy?",
+        answer:
+          "The capacity of a physical system to do work or any aspect of native that allows change to occur.",
+      },
+      {
+        question: "What is Energy?",
+        answer:
+          "The capacity of a physical system to do work or any aspect of native that allows change to occur.",
+      },
+      {
+        question: "What is Energy?",
+        answer:
+          "The capacity of a physical system to do work or any aspect of native that allows change to occur.",
+      },
+      {
+        question: "What is Energy?",
+        answer:
+          "The capacity of a physical system to do work or any aspect of native that allows change to occur.",
+      },
+      {
+        question: "What is Energy?",
+        answer:
+          "The capacity of a physical system to do work or any aspect of native that allows change to occur.",
+      },
+      {
+        question: "What is Energy?",
+        answer:
+          "The capacity of a physical system to do work or any aspect of native that allows change to occur.",
+      },
+      {
+        question: "What is Energy?",
+        answer:
+          "The capacity of a physical system to do work or any aspect of native that allows change to occur.",
+      },
+      {
+        question: "What is Energy?",
+        answer:
+          "The capacity of a physical system to do work or any aspect of native that allows change to occur.",
+      },
+    ],
+  },
+];
+
+module.exports = data;

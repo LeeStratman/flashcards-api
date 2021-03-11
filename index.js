3a8623ae6a4b89213bbf08f9ce962d4612344a4e
Lee Stratman <stratmanlc10@gmail.com>
2021-03-11 07:35:08 -0600

    Add cors package to project.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const connectDB = require("./startup/db");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const collections = require("./routes/collections");
 const flashcards = require("./routes/flashcards");
@@ -8,6 +9,7 @@ const errorHandler = require("./errorHandler");
 connectDB();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/collections", collections);
 app.use("/api/collections/", flashcards);
 app.use(errorHandler);
